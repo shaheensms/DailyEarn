b5868bc52a536bcbbe3b171a9d17eff4201574ae
shaheensms <shaheensms111@gmail.com>
2019-10-17 22:17:13 +0600

    Initial commit

---
+++ app/src/main/java/com/metacoders/dailyearn/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.metacoders.dailyearn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginBtn = (Button) findViewById(R.id.login_btn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
