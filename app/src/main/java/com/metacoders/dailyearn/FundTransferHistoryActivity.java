c4cb570b7318e00b7233502133a97175d98e5566
shaheensms <shaheensms111@gmail.com>
2019-11-05 20:56:33 +0600

    history and home ui redesigned

---
+++ app/src/main/java/com/metacoders/dailyearn/FundTransferHistoryActivity.java
@@ -0,0 +1,14 @@
+package com.metacoders.dailyearn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FundTransferHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fund_transfer_history);
+    }
+}
---
+++ app/src/main/java/com/metacoders/dailyearn/MutualFundActivity.java
@@ -0,0 +1,14 @@
+package com.metacoders.dailyearn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MutualFundActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mutual_fund);
+    }
+}
