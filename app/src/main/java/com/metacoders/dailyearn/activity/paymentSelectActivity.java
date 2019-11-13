a7045fe35a007e2b4963aa371136db38d619ba21
shaheensms <shaheensms111@gmail.com>
2019-11-13 17:29:44 +0600

    xml and java payment button added

--- app/src/main/java/com/metacoders/dailyearn/activity/paymentSelectActivity.java
+++ app/src/main/java/com/metacoders/dailyearn/activity/paymentSelectActivity.java
@@ -30,23 +30,29 @@ import java.util.Date;
 import java.util.Locale;
 
 public class paymentSelectActivity extends AppCompatActivity {
-    String balance  , method ,fromAcc ;
-    ImageView pmMoney , netTeller , payPal , payza  , bitcoin ;
-    String msg = "" ;
-    Button proceedBtn ;
-    String Flag = null ;
+    String balance, method, fromAcc;
+    ImageView pmMoney, netTeller, skrill, localBank, bitcoin, coinBase, bkash, rocket, nagad, paytm;
+    String msg = "";
+    Button proceedBtn;
+    String Flag = null;
 
-    Dialog dialog ;
+    Dialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.payment_system_select);
 
-        pmMoney = findViewById(R.id.perFectMoneyImage) ;
-        netTeller = findViewById(R.id.netleerImage) ;
-        payPal = findViewById(R.id.paypalImage) ;
-        payza = findViewById(R.id.payzaImage) ;
-        bitcoin = findViewById(R.id.btcoinImage) ;
+        pmMoney = findViewById(R.id.perFectMoneyImage);
+        netTeller = findViewById(R.id.netleerImage);
+        skrill = findViewById(R.id.skrill);
+        localBank = findViewById(R.id.bank);
+        bitcoin = findViewById(R.id.btcoinImage);
+        coinBase = findViewById(R.id.coin_base);
+        bkash = findViewById(R.id.bkash);
+        rocket = findViewById(R.id.rocket);
+        nagad = findViewById(R.id.nagad);
+        paytm = findViewById(R.id.paytm);
 
 
         // setting the  button  .
@@ -57,21 +63,22 @@ public class paymentSelectActivity extends AppCompatActivity {
             }
         });
 
-        payza.setOnClickListener(new View.OnClickListener() {
+        localBank.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                triggerDialogue("Payza");
+                triggerDialogue("Local Bank");
 
             }
         });
 
-        payPal.setOnClickListener(new View.OnClickListener() {
+        skrill.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                triggerDialogue("Pay pal");
+                triggerDialogue("Skrill");
             }
         });
+
         netTeller.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -83,32 +90,67 @@ public class paymentSelectActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                triggerDialogue("Perfect Money") ;
+                triggerDialogue("Perfect Money");
+
+            }
+        });
+
+        coinBase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                triggerDialogue("Coin Base");
+
+            }
+        });
+
+        bkash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                triggerDialogue("Bkash");
+
+            }
+        });
+
+        rocket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                triggerDialogue("Rocket");
+
+            }
+        });
+
+        nagad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                triggerDialogue("Nagad");
+
+            }
+        });
+
+        paytm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                triggerDialogue("Paytm");
 
             }
         });
 
 
-
-
-
-
-
-
-
         Intent y = getIntent();
-        method = y.getStringExtra("method") ;
-        balance = y.getStringExtra("amount") ;
-        fromAcc = y.getStringExtra("fromAccount") ;
-        if(method.equals("withdraw"))
-        {
-            Flag = "withdraw" ;
+        method = y.getStringExtra("method");
+        balance = y.getStringExtra("amount");
+        fromAcc = y.getStringExtra("fromAccount");
+        if (method.equals("withdraw")) {
+            Flag = "withdraw";
 
-        }
-        else
-            {
+        } else {
 
-                Flag = "deposit" ;
+            Flag = "deposit";
 
         }
 
@@ -117,33 +159,33 @@ public class paymentSelectActivity extends AppCompatActivity {
 
     private void triggerDialogue(final String text) {
         Context context;
-         dialog = new Dialog(paymentSelectActivity.this);
+        dialog = new Dialog(paymentSelectActivity.this);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.deposit_dialogue);
-        Button submitBtn = dialog.findViewById(R.id.submitBtn) ;
-        Button cancelBtn = dialog.findViewById(R.id.cancelBtn) ;
-        TextInputEditText bal = dialog.findViewById(R.id.amountIn) ;
-        final TextInputEditText mail = dialog.findViewById(R.id.mailIn) ;
-        TextView header = dialog.findViewById(R.id.headerText) ;
+        Button submitBtn = dialog.findViewById(R.id.submitBtn);
+        Button cancelBtn = dialog.findViewById(R.id.cancelBtn);
+        TextInputEditText bal = dialog.findViewById(R.id.amountIn);
+        final TextInputEditText mail = dialog.findViewById(R.id.mailIn);
+        TextView header = dialog.findViewById(R.id.headerText);
 
 
-        header.setText("Please Enter Your "+text + " Email Adress ") ;
+        header.setText("Please Enter Your " + text + " Email Adress ");
         bal.setText(balance);
 
 
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String Mail = mail.getText().toString() ;
+                final String Mail = mail.getText().toString();
                 if (!TextUtils.isEmpty(Mail)) {
-                    final String   DATE = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
-                    DatabaseReference mref = FirebaseDatabase.getInstance().getReference() ;
+                    final String DATE = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
+                    DatabaseReference mref = FirebaseDatabase.getInstance().getReference();
                     // upload the  transfer req to the preferred db
-                    final String  key = mref.push().getKey() ;
+                    final String key = mref.push().getKey();
 
 
                     //TODO NEED UID HERE
-                    modelForTransactionDb modelForTransactionDb = new modelForTransactionDb(key , balance , "testId" , Mail ,DATE  , text) ;
+                    modelForTransactionDb modelForTransactionDb = new modelForTransactionDb(key, balance, "testId", Mail, DATE, text);
                     mref.child("transacDb").child(key).setValue(modelForTransactionDb).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
@@ -154,7 +196,7 @@ public class paymentSelectActivity extends AppCompatActivity {
 
                             //  String reason, String status, String date, String amount
 
-                            modelForHIstory modelForHIstoryf = new modelForHIstory(key  , key , "You Requested  "+ balance + "  Your Account"  , "Pending",DATE ,balance) ;
+                            modelForHIstory modelForHIstoryf = new modelForHIstory(key, key, "You Requested  " + balance + "  Your Account", "Pending", DATE, balance);
 
                             st.child(key).setValue(modelForHIstoryf).addOnCompleteListener(new OnCompleteListener<Void>() {
                                 @Override
@@ -163,14 +205,13 @@ public class paymentSelectActivity extends AppCompatActivity {
                                     dialog.dismiss();
                                     showToast("DONE!!");
                                 }
-                            }) ;
+                            });
 
 
                         }
                     });
 
-                }
-                else {
+                } else {
 
                     showToast("Mail Address is empty !!!");
                 }
@@ -193,7 +234,7 @@ public class paymentSelectActivity extends AppCompatActivity {
 
     }
 
-    private void showToast (String s ){
+    private void showToast(String s) {
 
         Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT)
                 .show();
