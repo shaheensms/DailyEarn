99cb49646ee340d0834bb1a6c72eb11d7008cfa5
shaheensms <shaheensms111@gmail.com>
2019-11-20 00:08:50 +0600

    referral

---
+++ app/src/main/java/com/metacoders/dailyearn/ReferralActivity.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) $today.year.This Code is written by Gamkiller .
+ */
+
+package com.metacoders.dailyearn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReferralActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_referral);
+    }
+}
